aa13531e31a6cdf8454643278272abb11eacb785
Norman Gamage <norman.gamage@gmail.com>
2024-07-14 00:27:18 +1200

    2769

---
+++ Rust/2769.rs
@@ -0,0 +1,10 @@
+/**
+ * 2769. Find the Maximum Achievable Number
+ * https://leetcode.com/problems/find-the-maximum-achievable-number/
+ */
+
+impl Solution {
+  pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+    return num + 2 * t;
+  }
+}
---
+++ TypeScript/2769.ts
@@ -0,0 +1,8 @@
+/**
+ * 2769. Find the Maximum Achievable Number
+ * https://leetcode.com/problems/find-the-maximum-achievable-number/
+ */
+
+function theMaximumAchievableX(num: number, t: number): number {
+  return num + 2 * t;
+}
