ed7a186441c8203f288128c0d1b542db9309a93f
Norman Gamage <norman.gamage@gmail.com>
2024-07-23 20:42:53 +1200

    2235

---
+++ Rust/2235.rs
@@ -0,0 +1,10 @@
+/**
+ * 2235. Add Two Integers
+ * https://leetcode.com/problems/add-two-integers/
+ */
+
+impl Solution {
+  pub fn sum(num1: i32, num2: i32) -> i32 {
+    return num1 + num2;
+  }
+}
---
+++ TypeScript/2235.ts
@@ -0,0 +1,8 @@
+/**
+ * 2235. Add Two Integers
+ * https://leetcode.com/problems/add-two-integers/
+ */
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
