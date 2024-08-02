4bd9acbbd2b5d24dc4475715aa19c7949eded3a9
Norman Gamage <norman.gamage@gmail.com>
2024-08-02 16:11:37 +1200

    1741

---
+++ SQL/1741.sql
@@ -0,0 +1,9 @@
+-- 1741. Find Total Time Spent by Each Employee
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+
+SELECT event_day AS day,
+  emp_id,
+  SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY event_day,
+  emp_id;
